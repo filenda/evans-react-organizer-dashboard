ac4a4eb9dd04d6ef1a7c0d9f78cd2034c11a1e81
Vinicius Rodrigues Filenga <vinicius.filenga@neon.com.br>
2019-12-08 21:08:10 -0300

    chore: before implementing marcella's features

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import M from 'materialize-css';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Home() {
   return <div>essa fera</div>;
