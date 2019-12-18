2053637df3a4b0443286c60e2fb47655ab9f4764
Vinicius Rodrigues Filenga <vinicius.filenga@neon.com.br>
2019-12-18 00:03:06 -0300

    feat: added main page on react and fixed sidenav plus header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+/* begin - third party styles */
 import 'materialize-css/dist/css/materialize.min.css';
+import 'animate.css/animate.min.css';
+import 'chartist/dist/chartist.min.css';
+/* end - third party styles */
+
+/* begin - third paty scripts */
+import 'chartist/dist/chartist.min';
+/* end - third paty scripts */
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
+import SideNav from './components/SideNav';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <SideNav />
       <Routes />
       <GlobalStyle />
     </BrowserRouter>
