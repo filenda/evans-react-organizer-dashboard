d1ce921a394ff500cf4badde3be6268334c40cb4
Vinicius Rodrigues Filenga <vinicius.filenga@neon.com.br>
2019-12-18 22:05:23 -0300

    chore: branch last commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ import 'chartist/dist/chartist.min.css';
 /* end - third party styles */
 
 /* begin - third paty scripts */
+import 'chart.js/dist/Chart.min';
 import 'chartist/dist/chartist.min';
+import 'chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min';
+import 'chartist-plugin-fill-donut/dist/chartist-plugin-fill-donut.min';
+import 'datatables.net/js/jquery.dataTables.min';
+import 'datatables.net-responsive/js/dataTables.responsive.min';
+import 'datatables.net-select/js/dataTables.select.min';
 /* end - third paty scripts */
 
 import GlobalStyle from './styles/global';
